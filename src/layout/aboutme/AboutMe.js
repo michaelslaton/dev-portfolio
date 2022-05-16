22accb3ab5e9057b5e1f2ea3a9a45f8aadee566c
michaelslaton <michaelaaronphoto@gmail.com>
2022-05-15 21:35:25 -0700

    CSS adjustments

--- src/layout/aboutme/AboutMe.js
+++ src/layout/aboutme/AboutMe.js
@@ -4,12 +4,20 @@ export default function AboutMe(){
 
 
   return (
-    <div className="base-container">
+    <div className="bigolblock">
+      <div className="insidetheblock">
         <h1>About Me</h1>
         <h4>Hey, I'm Mike.</h4>
         <p>
-          I am a Full-Stack Developer located in California. 
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum."
         </p>
+      </div>
     </div>
   )
 }
