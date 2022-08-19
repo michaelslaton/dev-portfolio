2f014bdd187f112d3b4d37e6f1328861cc7bf0e1
michaelslaton <michaelaaronphoto@gmail.com>
2022-08-18 17:29:49 -0700

    Continued work & refinement on ui animations

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -3,13 +3,11 @@ import React from "react";
 import { useInView } from 'react-intersection-observer';
 
 import Navbar from './navbar/Navbar';
-import Footer from './footer/Footer';
-
 import AboutMe from './aboutme/AboutMe';
 import Skills from './skills/Skills';
 import Projects from './projects/Projects';
 import Contact from './contact/Contact';
-
+import Footer from './footer/Footer';
 
 export default function Layout(){
   const { ref: navRef, inView: visible } = useInView();
@@ -25,7 +23,6 @@ export default function Layout(){
 
       <div id="skills" className="anchor"/>
       <section><Skills/></section>
-
       
       <div id="projects" className="anchor"/>
       <section><Projects/></section>
